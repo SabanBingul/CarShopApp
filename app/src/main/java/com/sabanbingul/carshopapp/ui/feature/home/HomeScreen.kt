0ef812b0fd1e861c4e9c3edb6d95a90a86b2f3ad
Şaban Bingül <sabanbngl00@gmail.com>
2025-09-13 18:54:24 +0300

    Header in Dashboard

---
+++ app/src/main/java/com/sabanbingul/carshopapp/ui/feature/home/HomeScreen.kt
@@ -0,0 +1,28 @@
+package com.sabanbingul.carshopapp.ui.feature.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun MainScreen(){
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(color = 0xffeffefef))
+        ) {
+            item {
+                HeaderSection (username = "Alex Johnson", onBellClick = {})
+            }
+        }
+    }
+}
