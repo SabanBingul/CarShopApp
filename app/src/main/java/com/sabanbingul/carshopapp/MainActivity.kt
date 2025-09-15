2685e16808a6dd6d8497739238dc82f08fc5ed0f
Şaban Bingül <sabanbngl00@gmail.com>
2025-09-16 01:49:18 +0300

    Category Section added

--- app/src/main/java/com/sabanbingul/carshopapp/MainActivity.kt
+++ app/src/main/java/com/sabanbingul/carshopapp/MainActivity.kt
@@ -11,37 +11,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.sabanbingul.carshopapp.ui.feature.home.MainScreen
 import com.sabanbingul.carshopapp.ui.theme.CarShopAppTheme
+import com.sabanbingul.carshopapp.viewmodel.CategoryViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CarShopAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            val categoryViewModel : CategoryViewModel = viewModel()
+            MainScreen(categoryViewModel)
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CarShopAppTheme {
-        Greeting("Android")
-    }
-}
